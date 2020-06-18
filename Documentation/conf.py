441666bafe93f9e7906e25127f4a018321675261
Alex Solovev <alex.solovev.work@gmail.com>
2020-06-18 13:20:28 +0800

    Do not include docsearch assets

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -237,8 +237,8 @@ default_role = 'any'
 
 def setup(app):
     app.add_stylesheet('parsed-literal.css')
-    app.add_stylesheet('docsearch.min.css')
+    # app.add_stylesheet('docsearch.min.css')
     app.add_stylesheet('copybutton.css')
     app.add_javascript('clipboardjs.min.js')
     app.add_javascript("copybutton.js")
-    app.add_javascript('docsearch.min.js')
+    # app.add_javascript('docsearch.min.js')
